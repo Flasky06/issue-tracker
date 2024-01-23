dc519df2b8ea907a25fd5ba5e899cfce321a052f
flasky06 <bonnienjuguna106@gmail.com>
2024-01-23 11:13:46 +0300

    styled active links in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-import { FaBug } from "react-icons/fa";
+import classnames from "classnames";
+import { AiFillBug } from "react-icons/ai";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -10,14 +17,22 @@ const Navbar = () => {
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        <FaBug />
+        <AiFillBug />
       </Link>
 
       <ul className="flex space-x-6 ">
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            // className={`${
+            //   link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+            // }  hover:text-zinc-800 transition-colors`}
+
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
